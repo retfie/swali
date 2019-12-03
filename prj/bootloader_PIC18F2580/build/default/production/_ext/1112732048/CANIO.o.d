92bc0ed2b763f8922b679f4633117c3077c6c0bc
Maarten Zanders <maarten.zanders@gmail.com>
2019-12-04 00:42:15 +0100

    Initial commit

---
+++ prj/bootloader_PIC18F2580/build/default/production/_ext/1112732048/CANIO.o.d
@@ -0,0 +1,4 @@
+build/default/production/_ext/1112732048/CANIO.o:  \
+	D:/Maza/workspace/VSCP/SWALI/src/bootloader_pic18f2580/CANIO.asm  \
+	D:/Maza/workspace/VSCP/SWALI/src/bootloader_pic18f2580/CANIO.def  \
+
